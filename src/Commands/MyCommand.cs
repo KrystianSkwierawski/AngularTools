df29ce14fb0c881e117701a27f81af72d5cf54d6
bezi25251@gmail.com <bezi25251@gmail.com>
2024-11-28 20:01:04 +0100

    init

---
+++ src/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularTools
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("AngularTools", "Button clicked");
+        }
+    }
+}
