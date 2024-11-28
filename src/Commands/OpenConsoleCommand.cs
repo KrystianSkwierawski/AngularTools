1ef7bfe6c07dd7ee8aeb67ebf842065d7614948f
bezi25251@gmail.com <bezi25251@gmail.com>
2024-11-28 21:36:27 +0100

    add penConsoleCommand

---
+++ src/Commands/OpenConsoleCommand.cs
@@ -0,0 +1,23 @@
+﻿using EnvDTE;
+
+namespace AngularTools;
+
+[Command(PackageIds.OpenConsoleCommand)]
+internal sealed class OpenConsoleCommand : BaseCommand<OpenConsoleCommand>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        await ExtensionHelper.CatchErrorAsync(async () =>
+        {
+            var path = await ExtensionHelper.GetActiveProjectAsync();
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentNullException("Empty directory path");
+            }
+
+            var cmd = $"/k cd {path}";
+            System.Diagnostics.Process.Start("CMD.exe", cmd);
+        });
+    }
+}
---
+++ src/ExtensionHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace AngularTools;
+public static class ExtensionHelper
+{
+    public static async Task<string> GetActiveProjectAsync()
+    {
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+        var dte2 = await VS.GetServiceAsync<SDTE, DTE2>();
+        var activeSolutionProjects = dte2.ActiveSolutionProjects as Array;
+
+        if (activeSolutionProjects?.Length > 0)
+        {
+            var activeProject = activeSolutionProjects.GetValue(0) as EnvDTE.Project;
+            var fullname = activeProject.FullName;
+
+            var attr = File.GetAttributes(fullname);
+
+            return attr.HasFlag(FileAttributes.Directory) ? fullname : Path.GetDirectoryName(fullname);
+        }
+
+        return string.Empty;
+    }
+
+    public static async Task CatchErrorAsync(Func<Task> func)
+    {
+        try
+        {
+            await func();
+        }
+        catch (Exception ex)
+        {
+            ActivityLog.LogError($"[AngularTools]{nameof(OpenConsoleCommand)}", ex.ToString());
+            System.Windows.Forms.MessageBox.Show(ex.Message, $"[AngularTools]:{nameof(OpenConsoleCommand)} Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
