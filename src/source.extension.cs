088b481ff88cde3ddab02a12da0fbc8a1731d2da
bezi25251@gmail.com <bezi25251@gmail.com>
2025-04-29 17:46:25 +0200

    update metdata

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,36 +9,8 @@ namespace AngularTools
     {
         public const string Id = "AngularTools.1ddcbd14-b2ef-4ba0-bff8-d2b27660b977";
         public const string Name = "AngularTools";
-        public const string Description = @"# Angular Tools for Visual Studio 2022 (WIP)
-
-**Angular Tools** is a Visual Studio extension designed to streamline Angular development. It provides a set of productivity-enhancing commands and features tailored for Angular projects, making your workflow faster and more efficient.
-
-## Features
-
-1. **Rename Angular Component**  
-   Easily rename Angular components, including their associated files (`.ts`, `.html`, `.scss`, `.spec.ts`) and references across the project.
-
-*`Edit > Rename Angular Component`*
-
-2. **Switch Between `.ts` and `.html` Files**  
-   Quickly toggle between `.ts` and `.html`.
-
-*`Tools > Switch Between .ts/html` or `[shortcut Ctrl + 2]`*
-
-3. **Switch Between `.scss` and `.html` Files**  
-   Quickly toggle between `.scss` and `.html`.
-
-*`Tools > Switch Between .scss/.html` or `[shortcut Ctrl + 3]`*
-
-4. **Open Console in Project Directory**  
-   Open a terminal directly in the current project directory for quick access to Angular CLI commands.
-
-*`Tools > Open Console Here` or `[shortcut Ctrl + 4]`*
-
-## License
-
-This project is licensed under the [MIT License](LICENSE).
-";
+        public const string Description = @"Visual Studio extension designed to streamline Angular development. It provides a set of productivity-enhancing commands and features tailored for Angular projects, making your workflow faster and more efficient.
+        ";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Krystian Skwierawski";
